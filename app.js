f33b0051ec3242fc3e041fb8703f891521dc12cd
PainOchoco <matteo.planchet@gmail.com>
2020-04-21 01:04:13 +0200

    socket multiplayer positions setup

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const port = 7070;
+const io = require("socket.io")(server, {});
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/index.html");
 });
 
 app.use("/client", express.static(__dirname + "/client"));
-server.listen(7070);
+server.listen(port, () => {
+  console.log("Server listening on " + port);
+});
+
+let socketList = {};
+
+io.sockets.on("connection", (socket) => {
+  console.log("nouvelle connection");
+
+  socket.id = Math.random();
+  socket.x = 0;
+  socket.y = 0;
+
+  socketList[socket.id] = socket;
+});
+
+setInterval(() => {
+  let pack = [];
+  for (let i in socketList) {
+    let socket = socketList[i];
+    socket.x++;
+    socket.y++;
+    pack.push({
+      x: socket.x,
+      y: socket.y,
+    });
+
+    socket.emit("newPos", pack);
+  }
+}, 1000 / 25);
