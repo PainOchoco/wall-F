db562fcd09fac74b94e47b1e56355dbbca47bc5c
PainOchoco <matteo.planchet@gmail.com>
2020-04-21 12:23:41 +0200

    added Player & Entity classes

--- app.js
+++ app.js
@@ -15,27 +15,113 @@ server.listen(port, () => {
 
 let socketList = {};
 
+let Entity = () => {
+  let self = {
+    x: 250,
+    y: 250,
+    speedX: 0,
+    speedY: 0,
+    id: "",
+  };
+  self.update = () => {
+    self.updatePos();
+  };
+  self.updatePos = () => {
+    self.x += self.speedX;
+    self.y += self.speedY;
+  };
+  return self;
+};
+
+let Player = (id) => {
+  let self = Entity();
+  self.id = id;
+  self.number = "" + Math.floor(10 * Math.random());
+  self.pressRight = false;
+  self.pressLeft = false;
+  self.pressUp = false;
+  self.pressDown = false;
+  self.maxSpeed = 10;
+
+  let superUpdate = self.update;
+  self.update = () => {
+    self.updateSpeed();
+    superUpdate();
+  };
+
+  self.updateSpeed = () => {
+    if (self.pressRight) self.speedX = self.maxSpeed;
+    else if (self.pressLeft) self.speedX = -self.maxSpeed;
+    else self.speedX = 0;
+
+    if (self.pressUp) self.speedY = -self.maxSpeed;
+    else if (self.pressDown) self.speedY = self.maxSpeed;
+    else self.speedY = 0;
+  };
+  Player.list[id] = self;
+  return self;
+};
+
+Player.list = {};
+Player.onConnect = (socket) => {
+  let player = Player(socket.id);
+
+  socket.on("keyPress", (data) => {
+    switch (data.input) {
+      case "right":
+        player.pressRight = data.state;
+        break;
+      case "left":
+        player.pressLeft = data.state;
+        break;
+      case "up":
+        player.pressUp = data.state;
+        break;
+      case "down":
+        player.pressDown = data.state;
+        break;
+    }
+  });
+};
+
+Player.onDisconnect = (socket) => {
+  delete Player.list[socket.id];
+};
+
+Player.update = () => {
+  let pack = [];
+  for (let i in Player.list) {
+    let player = Player.list[i];
+    player.update();
+    pack.push({
+      x: player.x,
+      y: player.y,
+      number: player.number,
+    });
+  }
+  return pack;
+};
+
 io.sockets.on("connection", (socket) => {
-  console.log("nouvelle connection");
+  console.log("nouvelle connexion");
 
   socket.id = Math.random();
-  socket.x = 0;
-  socket.y = 0;
-
   socketList[socket.id] = socket;
+
+  Player.onConnect(socket);
+
+  socket.on("disconnect", () => {
+    console.log("déconnexion");
+    delete socketList[socket.id];
+    Player.onDisconnect(socket);
+  });
 });
 
 setInterval(() => {
-  let pack = [];
+  let pack = Player.update();
+
   for (let i in socketList) {
     let socket = socketList[i];
-    socket.x++;
-    socket.y++;
-    pack.push({
-      x: socket.x,
-      y: socket.y,
-    });
-
     socket.emit("newPos", pack);
   }
 }, 1000 / 25);
